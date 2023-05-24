7d3ef25cdb823076bb5ad7317d14abefb6521476
Haclio <62893532+Haclio@users.noreply.github.com>
2023-05-24 10:44:03 +0200

    Final update + plot

--- 2023-W21/Squirrels.r
+++ 2023-W21/Squirrels.r
@@ -1,8 +1,9 @@
 install.packages("janitor")
 install.packages("treemapify")
-library(sf)
-library(janitor)
-library(treemapify)
+install.packages("ggh4x")
+library(treemapify) #To make the treemap
+library(janitor) #Cleaning names
+library(ggh4x) #To switch facet strip position
 setwd("./2023-W21")
 
 # tuesdata <- tidytuesdayR::tt_load(2023, week = 21)
@@ -11,19 +12,28 @@ setwd("./2023-W21")
 squirrel_data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-05-23/squirrel_data.csv') |>
     clean_names()
 
-centralpark <- read_sf(dsn = "Map", layer  = "CentralPark")
-
-centralpark |>
-ggplot() +
-geom_sf() +
-geom_point(data = squirrel_data, aes(x = X, y = Y))
-
-ggplot(squirrel_data |> drop_na(highlight_fur_color)) +
-  geom_bar(aes(x = primary_fur_color, fill = highlight_fur_color),  position = position_fill()) +
-  coord_flip()
-
 squir_sum <- squirrel_data |>
-  summarize()
-ggplot(squirrel_data |> drop_na(highlight_fur_color), aes(area = highlight_fur_color, label = highlight_fur_color, subgroup = primary_fur_color)) +
-geom_treemap()
+  drop_na(primary_fur_color) |> #Drop the NAs
+  reframe(nb = n(), .by  = c("primary_fur_color", "highlight_fur_color")) |> # = summarize()
+  replace_na(list(highlight_fur_color = "None")) |> #Replace NAs by "None"
+  mutate(cols = case_when(highlight_fur_color == "Black" ~ "black", #Hard-coding color values for the plot, must be a better way to do this
+                          highlight_fur_color == "Gray" ~ "grey40",
+                          highlight_fur_color == "Cinnamon" ~ "#ca6e04",
+                          highlight_fur_color == "White" ~ "white",
+                          highlight_fur_color == "Gray, White" ~ "grey50",
+                          highlight_fur_color == "Gray, Black" ~ "grey20",
+                          highlight_fur_color == "Black, White" ~ "grey30",
+                          highlight_fur_color == "Cinnamon, White" ~ "#ec9634",
+                          highlight_fur_color == "Black, Cinnamon" ~ "#8f4d02",
+                          highlight_fur_color == "Black, Cinnamon, White" ~ "#ca6e04",
+                          highlight_fur_color == "None" & primary_fur_color == "Black" ~ "black",
+                          highlight_fur_color == "None" & primary_fur_color == "Gray" ~ "grey40",
+                          highlight_fur_color == "None" & primary_fur_color == "Cinnamon" ~ "#ca6e04"))
 
+ggplot(squir_sum) +
+geom_treemap(aes(area = nb, fill = I(cols)), show.legend = FALSE) + #I() necessary here to take the columns values as colors
+geom_treemap_text(aes(area = nb, label = highlight_fur_color), place = "center", color = ifelse(squir_sum$highlight_fur_color == "White", "black", "white"), reflow = TRUE) + #reflow for wrapping text
+facet_grid2(primary_fur_color ~ ., switch = "both", strip = strip_themed(background_y = list(element_rect(fill = "black"), element_rect(fill = "#ca6e04"), element_rect(fill = "grey40")))) + #switch = facet strip position
+xlab("Highlights fur color") +
+ylab("Primary fur color") +
+theme(axis.title = element_text(size = 30), strip.text.y = element_text(size = 20, color = "white"), text = element_text(family = "Verdana"), panel.border = element_rect(color = "grey20", fill = NA), strip.background = element_rect(color = "grey20", fill = NA))

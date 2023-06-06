1ecb2593797eb597e6ff5fd7442d63ee9dd0cd1d
Louis <62893532+Haclio@users.noreply.github.com>
2023-06-06 13:20:30 +0200

    Final commit week 23

--- 2023-W23/owid_energy.r
+++ 2023-W23/owid_energy.r
@@ -1,21 +1,20 @@
 library(tidyverse)
 library(stringr)
-library(sf)
 setwd("./2023-W23")
 mytheme <- theme_set(theme_classic() + theme(legend.background = element_rect(fill = "#fff6ec"), panel.background = element_rect(fill = "#fff6ec"),
 plot.background = element_rect(fill = "#fff6ec"), text = element_text(family = "Verdana"), legend.position = "bottom", axis.title.x = element_blank(), axis.text = element_text(size = 20),
     panel.grid.major = element_line(color = "black", linetype = "dashed"), axis.line = element_blank(),
     legend.text = element_text(size = 16, family = "Verdana"), legend.title = element_text(face = "bold", size = 16, family = "Verdana"),
-    axis.title.y = element_text(size = 20)))
+    axis.title.y = element_text(size = 20))) #Setting the ggplot theme
 
 owid_energy <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-06-06/owid-energy.csv')
 owid_energy_fr <- owid_energy |>    
-    filter(country == "France")
+    filter(country == "France") #Filtering data on France only
 owid_energy_fr_prod <- owid_energy_fr |>
-    select(year, ends_with("share_elec")) |>
-    select(-c(fossil_share_elec, renewables_share_elec, other_renewables_share_elec, low_carbon_share_elec)) |>
-    pivot_longer(cols = !year, names_to = "Energy_type", values_to = "share") |>
-    mutate(Energy_type = str_to_title(gsub("_share_elec", "", Energy_type)))
+    select(year, ends_with("share_elec")) |> #Removing unused columns
+    select(-c(fossil_share_elec, renewables_share_elec, other_renewables_share_elec, low_carbon_share_elec)) |> #Removing unused columns
+    pivot_longer(cols = !year, names_to = "Energy_type", values_to = "share") |> #Pivot to long format
+    mutate(Energy_type = str_to_title(gsub("_share_elec", "", Energy_type))) #Simplification and capitalization of column names
 
 ggplot(owid_energy_fr_prod) +
     geom_area(aes(x = year, y = share, fill = Energy_type), size = 1.5, alpha = 0.9) +
@@ -27,14 +26,14 @@ ggplot(owid_energy_fr_prod) +
     scale_x_continuous(limits = c(1985, 2022), breaks = seq(1985, 2022, 5), expand = c(0.01, 0.01)) +
     scale_y_continuous(limits = c(0, 100), breaks = seq(0, 100, 20), expand = c(0.01, 0.01))
 
-euro_countries <- codelist |>
-    filter(continent == "Europe") |>
-    select(3, 6)
+# euro_countries <- codelist |>
+#     filter(continent == "Europe") |>
+#     select(3, 6)
 
-owid_energy_euro <- subset(owid_energy, country %in% euro_countries$country.name.en) |>
-    filter(year == "2022") |>
-    select(c(country, electricity_generation, electricity_demand)) |>
-    pivot_longer(cols = !country, names_to = "Type", values_to = "Value")
+# owid_energy_euro <- subset(owid_energy, country %in% euro_countries$country.name.en) |>
+#     filter(year == "2022") |>
+#     select(c(country, electricity_generation, electricity_demand)) |>
+#     pivot_longer(cols = !country, names_to = "Type", values_to = "Value")
 
-ggplot(owid_energy_euro) +
-    geom_bar(aes(x = country, y = Value, fill = Type), position = "dodge", stat = "identity")
+# ggplot(owid_energy_euro) +
+#     geom_bar(aes(x = country, y = Value, fill = Type), position = "dodge", stat = "identity")

1ffebe085e1e0e14224337be53dd978672f1c7d3
Louis <62893532+Haclio@users.noreply.github.com>
2023-07-04 17:38:40 +0200

    Final commit week 27

--- 2023-W27/Historicalplaces.r
+++ 2023-W27/Historicalplaces.r
@@ -1,11 +1,15 @@
 setwd("./2023-W27")
 library(tidyverse)
-library(png)
 # install.packages("ggtext")
 library(ggtext)
-library(extrafont)
-font_import()
-loadfonts(device = "win")
+library(showtext)
+showtext_opts(dpi = 320)
+showtext_auto(enable = TRUE)
+font_add(family = "fb", regular = "Font Awesome 6 Brands-Regular-400.otf") #Brand logos
+font_add(family = "pl", regular = "C:/Windows/Fonts/pala.ttf") #Palatino Linotype
+
+cap <- paste0("<span style='font-family:fb;'>&#xf09b; </span> Haclio  |", #Caption
+              "<span style='font-family:fb;'> &#xf099; </span>@LouisNadalin | Source: Historical Markers Database | #TidyTuesday week 27")
 
 historical_markers <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-07-04/historical_markers.csv')
 # no_markers <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-07-04/no_markers.csv')
@@ -59,27 +63,35 @@ df <- rbind(bee, firebug, grasshopper, locust, louse, mosquito, sandfly, wasp, w
 df_sum <- df |>
   reframe(n = n(), .by = type)
 fct_relevel(df_sum$type, c("Bee", "Locust", "Grasshopper", "Mosquito", "Firebug", "Louse", "Sandfly", "Wasp", "Weevil"))
+search <- c(paste0("\"Bee\"", ", \"beehive\",", "\n \"bee-hive\"", ", \"Beeville\""), paste("\"Firebug\""), paste("\"Grasshopper\""), paste("\"Locust\""), paste("\"Louse\""), paste("\"Mosquito\""), paste("\"Sandfly\""), paste("\"Women Airforce \n Service Pilots (WASP)\""), paste("\"Weevil\""))
+df_sum$search <- search
+df_sum <- rbind(c("", NA, "Query :"), df_sum) |>
+  mutate(n = as.numeric(n))
 
-label <- c(
-  Weevil = "<img src='weevilimg.png' height = 65>",
-  Wasp = "<img src='waspimg.png' height = 65>",
-  Sandfly = "<img src='sandfly.png' height = 65>",
-  Locust = "<img src='locust.png' height = 65>",
-  Louse = "<img src='louse.png' height = 65>",
-  Mosquito = "<img src='mosquito.png' height = 65>",
-  Bee = "<img src='bee.png' height = 65>",
-  Firebug = "<img src='firebug.png' height = 65>",
-  Grasshopper = "<img src='grasshopper.png' height = 65>")
 
-ggplot(df_sum, aes(x = reorder(fct_rev(type), n))) +
+label <- c( #Array of pictures to use as axis labels
+  Weevil = "<img src='weevilimg.png' height = 60>",
+  Wasp = "<img src='waspimg.png' height = 60>",
+  Sandfly = "<img src='sandfly.png' height = 60>",
+  Locust = "<img src='locust.png' height = 60>",
+  Louse = "<img src='louse.png' height = 60>",
+  Mosquito = "<img src='mosquito.png' height = 60>",
+  Bee = "<img src='bee.png' height = 60>",
+  Firebug = "<img src='firebug.png' height = 60>",
+  Grasshopper = "<img src='grasshopper.png' height = 60>")
+
+ggplot(df_sum, aes(x = reorder(fct_rev(type), n))) + #fct_rev to order from max to min
     geom_col(aes(y = n), width = 0.8, fill = "#d1941b") +
-    geom_text(aes(y = n -0.5, label = n), color = "#10290d", size = 14, family = "Perpetua", fontface = "bold") +
-    geom_text(aes(y = n + 0.5, label = type, hjust = 0), color = "grey60", size = 12, family = "serif", fontface = "italic") +
-    coord_flip() +
+    geom_text(aes(y = n - 0.25, label = n, hjust = 1), color = "#071306", size = 12, fontface = "bold") +
+    geom_text(aes(y = n + 0.8, label = type, hjust = 0), color = "grey60", size = 12, family = "serif", fontface = "italic") +
+    geom_text(aes(y = 20, label = search), color = "#b7cfb5", family = "serif", fontface = "italic", size = 5) +
+    coord_flip() + #Rotate the graph
     scale_x_discrete(name = NULL, labels = label) +
-    scale_y_continuous(expand = expansion(mult = c(0.5,0.5))) +
-    # ylim(expand = c(0.5, 0.5)) + #ERROR
+    scale_y_continuous(expand = expansion(mult = c(0.05, 0.1))) +
+    labs(title = "Historical (bug) markers", subtitle = "Bugs in historical markers names", caption = cap) +
     theme_void() +
-    theme(axis.title = element_blank(), axis.text.y = element_markdown(size = 10, margin = margin(0, -30, 0, 0)),
-    plot.background = element_rect(fill = "#10290d", color = NA), panel.background = element_rect(fill = "#10290d", color = NA),
-    plot.margin = margin(1,1,1.5,1.2, "cm"))
+    theme(axis.title = element_blank(), axis.text.y = element_markdown(size = 10, margin = margin(0, -30, 0, 0)), #element_markdown necessary for the pictures to show
+    plot.background = element_rect(fill = "#071306", color = NA), panel.background = element_rect(fill = "#071306", color = NA),
+    plot.margin = margin(1, 1, 1, 1.2, "cm"), plot.caption = element_markdown(color = "#b7cfb5", margin = margin(t = 10, b = 0), size = 12, vjust = -10), #element_markdown necessary for the logos to appear
+    plot.title = element_text(hjust = 0.5, colour = "#f8c96c", family = "pl", size = 50, face = "bold"),
+    plot.subtitle = element_text(hjust = 0.5, colour = "grey60", family = "pl", size = 25, face = "bold", vjust = 0.1))

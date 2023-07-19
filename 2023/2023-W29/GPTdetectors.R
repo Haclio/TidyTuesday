48cfb3ee5ee8d833668ca5ac7d980622550d23db
Haclio <62893532+Haclio@users.noreply.github.com>
2023-07-19 18:42:09 +0200

    Last commit week 29

--- 2023/2023-W29/GPTdetectors.R
+++ 2023/2023-W29/GPTdetectors.R
@@ -6,6 +6,9 @@ library(ggtext)
 # library(ggwaffle)
 # install.packages("waffle", repos = "https://cinc.rud.is")
 library(waffle)
+# install.packages("ggh4x")
+# library(ggh4x)
+library(patchwork)
 showtext_opts(dpi = 320)
 showtext_auto(enable = TRUE)
 font_add(family = "fb", regular = paste(dirname(dirname(getwd())), "Font Awesome 6 Brands-Regular-400.otf", sep = "/")) #Brand logos in parent (x2) directory
@@ -16,25 +19,71 @@ detectors <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/
 det_sum <- detectors |>
     summarize(n = n(), .by = c(".pred_class", "native")) |>
     filter(!is.na(native)) |>
-    group_by(.pred_class) |>
+    group_by(native) |>
     mutate(pct = round(n/sum(n)*100)) |>
-    arrange(.pred_class, native)
+    mutate(native = factor(native, levels = c("Yes", "No"))) |>
+    arrange(native, .pred_class)
 
-ggplot(det_sum) +
-geom_waffle(aes(values = pct, fill = native), na.rm = FALSE, flip = TRUE, radius = unit(20, "pt"), size = 0) +
-facet_grid(. ~ .pred_class, scales = "free") +
+det_bydet <- detectors |>
+    filter(!is.na(native) & native == "No") |>
+    summarize(n = n(), .by = c(".pred_class", "detector")) |>
+    group_by(detector) |>
+    mutate(pct = round(n/sum(n)*100)) |>
+    arrange(detector, .pred_class) |>
+    mutate(lab = ifelse(.pred_class == "AI", paste(detector, ":", pct, "%"), NA)) |>
+    group_by(detector) |>
+    fill(lab, .direction = "down")
+
+toplabs <- data.frame(native = c("Yes", "No"), lab = c("Out of 1831 texts written by native English writers, <br><span style = 'color: #daaa32;'>**97%**</span> were classified as <span style = 'color: #daaa32;'>**written by a human**</span>...", "Out of 637 texts written by non-native English writers, <br><span style = 'color: #daaa32;'>**only 39%**</span> were classified as <span style = 'color: #daaa32;'>**written by a human**</span>..."))
+bottomlabs <- data.frame(native = c("Yes", "No"), lab = c("...while only <span style = 'color: grey70;'>**3%**</span> were classified as <span style = 'color: grey70;'>**AI-generated**</span>.", "...while <span style = 'color: grey70;'>**61%**</span> were classified as <span style = 'color: grey70;'>**AI-generated**</span>."))
+
+gg1 <- ggplot(det_sum) +
+geom_waffle(aes(values = pct, fill = .pred_class), na.rm = FALSE, flip = TRUE, radius = unit(10, "pt"), size = 0, show.legend = FALSE) +
+facet_grid(. ~ rev(native), scales = "free") +
 theme_void() +
+ylim(-4, 14) +
+xlim(-4, 14) +
 theme(aspect.ratio = 1) +
-labs(title = "The Earth is on <span style = 'color: darkorange1;'>**fire**</span>", subtitle = "Temperature deviations from the 1951-1980 monthly average", caption = cap) +
+labs(title = "GPT detectors disproportionately classify \nnon-native English writing as AI-generated") +
+geom_richtext(data = toplabs, aes(x = -3, y = 12, label = lab), color = "white", hjust = 0, size = 3.8, family = "vd", label.color = NA, fill = NA) +
+geom_richtext(data = bottomlabs, aes(x = -3, y = -1, label = lab), color = "white", hjust = 0, size = 3.8, family = "vd", label.color = NA, fill = NA) +
+scale_fill_manual(values = c("grey50", "#CD950C")) +
 theme(plot.background = element_rect(fill = "#25251f", color = NA), panel.background = element_rect(fill = "#25251f", color = NA),
-text = element_text(color = "white", family = "vd"),
-panel.grid.minor = element_blank(), legend.background = element_rect(fill = "#25251f", color = NA),
-legend.position = "bottom",plot.margin = margin(1, 1, 1, 1.2, "cm"), legend.key.width = unit(1.5, "cm"), 
-legend.text = element_text(size = 12), legend.title = element_text(size = 16),
-plot.caption = element_markdown(color = "white", margin = margin(t = 30), size = 8, hjust = 1),
-plot.title = element_markdown(size = 30, family = "vd", margin = margin(t = 20, b = 10)),
-plot.subtitle = element_text(size = 16, family = "vd", margin = margin(t = 10, b = 20)))
+text = element_text(color = "white", family = "vd"), plot.margin = margin(-1, -1.1, -50, -1.2, "cm"),
+strip.text.x = element_blank(), plot.title = element_text(size = 30, family = "vd", margin = margin(t = 20, b = 10)))
 
-ggplot(det_sum) +
-geom_col(aes(x = .pred_class, y = pct, fill = native))
-#333324
+ggCGHO <- ggplot(det_bydet |> filter(detector == "GPTZero" | detector == "HFOpenAI" | detector == "OriginalityAI" | detector == "Crossplag")) +
+geom_waffle(aes(values = pct, fill = .pred_class), na.rm = FALSE, flip = TRUE, radius = unit(6, "pt"), size = 0, show.legend = FALSE) +
+theme_void() +
+theme(aspect.ratio = 0.8, strip.text.x = element_text(size = 12, color = "white", family = "vd", face = "bold", vjust = 2, hjust = 0.55, margin = margin(10,0,10,0, "pt"))) +
+xlim(-1, 11) +
+coord_fixed() +
+scale_fill_manual(values = c("grey50", "#CD950C")) +
+facet_wrap(. ~ lab, nrow = 1)
+
+
+ggQSZ <- ggplot(det_bydet |> filter(detector == "Quil" | detector == "Sapling" | detector == "ZeroGPT")) +
+geom_waffle(aes(values = pct, fill = .pred_class), na.rm = FALSE, flip = TRUE, radius = unit(6, "pt"), size = 0, show.legend = FALSE) +
+theme(aspect.ratio = 1) + 
+coord_fixed() +
+xlim(-1, 11) +
+scale_fill_manual(values = c("grey50", "#CD950C")) +
+facet_wrap(. ~ lab, nrow = 1)
+
+emptyplot <- ggplot()
+gg2 <- (emptyplot + ggQSZ + emptyplot + plot_layout(widths = c(0.04, 0.92, 0.04))) &
+ theme_void() & 
+ theme(strip.text.x = element_text(size = 12, color = "white", family = "vd", face = "bold", vjust = 2, hjust = 0.55,
+ margin = margin(10,0,10,0, "pt")))
+
+gg3 <- ggCGHO / gg2 +
+plot_annotation(title = "Some detectors do better than others :", cap = "Attribution of non-native English writing as AI-generated") &
+theme(plot.background = element_rect(fill = "#25251f", color = NA), panel.background = element_rect(fill = "#25251f", color = NA), plot.margin = margin(0.2, 0, 0.2, 0, "cm"), 
+plot.title = element_text(size = 20, family = "vd", margin = margin(t = -50, b = 10), color = "white"),
+plot.caption = element_markdown(color = "white", size = 14, hjust = 0.5, face = "bold", family = "vd"))
+
+gg1 / wrap_elements(gg3) + plot_layout(heights = c(1, 1.2)) +
+plot_annotation(caption = cap) &
+theme(plot.background = element_rect(fill = "#25251f", color = NA), panel.background = element_rect(fill = "#25251f", color = NA),
+text = element_text(color = "white", family = "vd"), plot.margin = margin(1, 1.1, 1, 1.2, "cm"), 
+plot.caption = element_markdown(color = "white", margin = margin(t = 30), size = 10, hjust = 1))

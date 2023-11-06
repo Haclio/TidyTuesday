8b7b80780fd798a1617886707946c3eac97aca8e
Louis <62893532+Haclio@users.noreply.github.com>
2023-11-06 23:12:04 +0100

    Update elections.R

--- 2023/2023-W45/elections.R
+++ 2023/2023-W45/elections.R
@@ -11,71 +11,71 @@ font_add(family = "ssp", regular = paste(fontsfolder, "Source_Sans_Pro", "Source
                          bold = paste(fontsfolder, "Source_Sans_Pro", "SourceSansPro-Bold.ttf", sep = "/"),
                          italic = paste(fontsfolder, "Source_Sans_Pro", "SourceSansPro-Italic.ttf", sep = "/")) #Source Sans Pro
 
-cap <- "<span style='font-family:fb;'>&#xf09b; </span> Haclio  |  <span style='font-family:fb;'> &#xf099; </span>@LouisNadalin | Bluesky : @louisnadalin.bsky.social | Source: Grants.gov website | #TidyTuesday week 45 2023"
-sub <- "According to grants.gov, 'A grant is a way the government funds your ideas and projects<br>
-        to provide public services and stimulate the economy. Grants support critical recovery<br>
-        initiatives, innovative research, and many other programs.'<br><br>
-        Grants can fall under four main types depending on how they're attributed.<br>
-        Below is shown the distribution of the 2000 grants we had data on this week,<br>
-        by their type and category. Note that some grants fall under several categories."
+cap <- "<span style='font-family:fb;'>&#xf09b; </span> Haclio  |  <span style='font-family:fb;'> &#xf099; </span>@LouisNadalin | Bluesky : @louisnadalin.bsky.social | Source: MIT Election Data and Science Lab | #TidyTuesday week 45 2023"
+sub <- "The United States House of Representatives is the lower chamber of the US Congress, the Senate being the upper chamber.<br>
+        The purpose of both chambers is to pass federal legislation, better known as bills. The House also has dedicated powers,<br>
+        such as originating revenue bills, or initiating impeachment proceedings.<br><br>
+        Each state's plot below represents the affiliation of its representatives in each even-year election,<br>
+        from 1976 to 2022, between <span style='color: #4fa7d3'>**Democrats**</span>, <span style='color: #ea5d56'>**Republicans**</span>, and <span style='color: #739627'>**other affiliations**</span>.<br>
+        <span>*Note that the District of Columbia is shown here but its representative doesn't have any voting power in the House.*</span>"
 
 #Data wrangling
 house <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-11-07/house.csv')
 house2 <- house |>
-    filter(stage == "GEN" & special == FALSE) |>
-    # filter(!state_po == "DC") |>
+    filter(stage == "GEN" & special == FALSE) |> #Only keep regular general elections
+    filter(!state_po == "DC") |> #Remove DC, only one data point
     mutate(party = as.factor(party)) |>
-    mutate(party = fct_other(party, keep = c("DEMOCRAT", "REPUBLICAN"))) |>
+    mutate(party = fct_other(party, keep = c("DEMOCRAT", "REPUBLICAN"))) |> #Collapses non-D, non-R levels into 'other'
     mutate(pct = candidatevotes / totalvotes * 100) |>
-    slice_max(pct, by = c("year", "state", "district")) |>
+    slice_max(pct, by = c("year", "state", "district")) |> #Keep the maximum value per district = the winner of the election
     summarize(n = length(candidate), .by = c(year, state_po, party)) |>
     group_by(year, state_po) |>
-    mutate(pct = n / sum(n) * 100) |>
+    mutate(pct = n / sum(n) * 100) |> #% of D/R/misc candidates
     rename(id = state_po)
 
-us <- read_sf("us_states_hexgrid.geojson")
+us <- read_sf("us_states_hexgrid.geojson") #Hexagon US base map
 
-us_map <- fortify(us, region="iso3166_2") |>
-    st_transform(crs="+init=epsg:3857")
+us_map <- fortify(us, region = "iso3166_2") |> #isoxxx = state name abbreviations
+    st_transform(crs="+init=epsg:3857") #To make the hexagons into the right shape
 
 centers <- cbind.data.frame(data.frame(st_centroid(us_map), id=us$iso3166_2)) |>
-    select(geometry, id)
+    select(geometry, id) #Centroid calculations for plot locations
 house3 <- merge(house2, centers)
-house3 <- cbind(house3, data.frame(st_coordinates(st_cast(house3$geometry))))
-house3 <- bind_rows(house3, data.frame(year = c(seq(1976, 2018, 2), 2022), id = "DC", X = -8993986.841, Y = 4447891.376, pct = 0))
+house3 <- cbind(house3, data.frame(st_coordinates(st_cast(house3$geometry)))) #centroids geometry as numbers to calculate easily
 
-us_map1 <- ggplot() +
-    geom_sf(data = us_map, color = "grey80", linewidth = 0.8, fill = "#052639") +
-    geom_sf_text(data = us_map, aes(label = iso3166_2, geometry = geometry), color = "grey90", size = 4, vjust = -1.8, family = "ssp", fontface = "bold") +
-    labs(title = "Haunted places of the USA", subtitle = sub, caption = cap) + #Title, subtitle, caption
+us_map1 <- ggplot() + #Base hexagon map with title, subtitle and caption
+    geom_sf(data = us_map, color = "grey80", linewidth = 0.8, fill = ifelse(us_map$iso3166_2 == "DC", "grey55", "#052639")) +
+    geom_sf_text(data = us_map, aes(label = iso3166_2, geometry = geometry), color = "grey90", size = 3.5, vjust = -2.4, family = "ssp", fontface = "bold") +
+    labs(title = "US House of Representatives distribution<br>1976 to 2022", subtitle = sub, caption = cap) + #Title, subtitle, caption
     theme_void() +
-    theme(plot.background = element_rect(fill = "grey25", color = NA), #Background color
-          plot.caption = element_markdown(margin = margin(10, 0, -5, 0), size = 12, color = "grey90"), #Caption formatting
+    theme(plot.background = element_rect(fill = "grey98", color = NA), #Background color
+          plot.caption = element_markdown(margin = margin(10, -15, -5, 0), size = 10, color = "grey20"), #Caption formatting
           plot.margin = unit(c(1,1,0.5,0.5),"cm"), #Margins
-          plot.subtitle = element_markdown(family = "ssp", hjust = 0.5, halign = 0.5, size = 12, lineheight = 1.2, color = "grey90"), #Subtitle settings
-          plot.title = element_markdown(size = 30, margin = margin(b = 25), hjust = 0.5, color = "grey90"), #Title settings
+          plot.subtitle = element_markdown(family = "ssp", hjust = 0.5, halign = 0.5, size = 12, lineheight = 1.2, color = "grey20"), #Subtitle settings
+          plot.title = element_markdown(size = 28, margin = margin(b = 10), hjust = 0.5, color = "grey20", lineheight = 1.2), #Title settings
           plot.title.position = "plot")
 us_map1
 
-for (i in unique(house3$id)) { 
+for (i in unique(house3$id)) { #Loop over the states to create each subplot, then add it to the main plot with calculated coordinates
     state_data <- house3[house3$id == i, ]
 
     p <- ggplot() +
+    geom_rect(data = house3, aes(xmin = 1974, xmax = 2024, ymin = -2, ymax = 102), color = ifelse(house3$id == "DC", NA, "white"), fill = NA, linewidth = 3) +
     geom_col(data = state_data, aes(x = year, y = pct, fill = party), show.legend = FALSE, width = 2.5, color = NA) +
-    scale_x_discrete(drop=FALSE) +
-    scale_fill_manual(values = c("DEMOCRAT" = "#4fa7d3", "REPUBLICAN" = "#ea5d56", "Other" = "grey40")) +
+    scale_x_discrete(expand = c(0, 0), drop=FALSE) +
+    scale_y_continuous(expand = c(0, 0)) +
+    scale_fill_manual(values = c("DEMOCRAT" = "#5abced", "REPUBLICAN" = "#e45f58", "Other" = "olivedrab3")) +
     theme_void()
 
     g <- ggplotGrob(p)
 
-    Xmin <- mean(state_data[state_data$id == i, ]$X) - 250000
-    Xmax <- mean(state_data[state_data$id == i, ]$X) + 250000
-    Ymin <- mean(state_data[state_data$id == i, ]$Y) - 190000
-    Ymax <- mean(state_data[state_data$id == i, ]$Y) + 170000
+    Xmin <- mean(state_data[state_data$id == i, ]$X) - 240000
+    Xmax <- mean(state_data[state_data$id == i, ]$X) + 240000
+    Ymin <- mean(state_data[state_data$id == i, ]$Y) - 180000
+    Ymax <- mean(state_data[state_data$id == i, ]$Y) + 160000
     us_map1 <- us_map1 +
     annotation_custom(grob = g,  xmin = Xmin, xmax = Xmax, ymin = Ymin, ymax = Ymax)
 }
 us_map1
 
-us_map1 + annotation_custom(grob = g, xmin = Xmin, xmax = Xmax, ymin = Ymin, ymax = Ymax)
-"#393905"
+ggsave(filename = "house.png", width = 2052, height = 1800, units = "px", scale = 1.45)
